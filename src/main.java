10fe6e511522549a6674e71899895709aa545e50
Duha <duha.abu99@gmail.com>
2021-01-08 15:02:27 +0200

    test M

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Master");
         System.out.println("This is branch 1");
+        System.out.println("Test Master");
     }
 }
